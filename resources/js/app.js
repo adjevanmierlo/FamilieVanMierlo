4b80d7b2079dfba0b4d90cee5ea14aa2bb056860
Adje van Mierlo <1979adje@gmail.com>
2026-04-21 20:27:47 +0200

    eerste commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import "../scss/app.scss";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+Alpine.start();
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: false }" :class="{ 'dark': darkMode }">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Familie Van Mierlo') }}</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    @vite(['resources/js/app.js', 'resources/scss/app.scss'])
+    @livewireStyles
+</head>
+
+<body>
+    <div class="app-wrapper">
+        @include('layouts.navigation')
+        <main class="app-main">
+            {{ $slot }}
+        </main>
+    </div>
+    @livewireScripts
+</body>
+
+</html>
